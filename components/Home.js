8591ff70af69e3fa1e3e2dd07e94b582b16839ef
Michael Goodrum <michael@vlipsy.com>
2019-11-04 16:37:04 -0500

    add home screen with toggle for dark mode

---
+++ components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Button, Layout, Text} from 'react-native-ui-kitten';
+
+export const Home = props => (
+  <Layout style={styles.container}>
+    <Text category="h4">Welcome to UI Kitten</Text>
+    <Button onPress={props.toggleTheme}>Toggle Dark Mode 🌚</Button>
+  </Layout>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+});
